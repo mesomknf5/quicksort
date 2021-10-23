8033964b7a772f2bb351cc5f297cf129b97c7929
DeepanshuCSIT <93028563+DeepanshuCSIT@users.noreply.github.com>
2021-10-23 12:31:05 +0530

    Update qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -40,17 +40,17 @@ int partition (int arr[], int low, int high)
 arr[] --> Array to be sorted,
 low --> Starting index,
 high --> Ending index */
-void quickSort(int arr[], int low, int high)
+void quickSort(int arr[], int loww, int high)
 {
-	if (low < high)
+	if (loww < high)
 	{
 		/* pi is partitioning index, arr[p] is now
 		at right place */
-		int pi = partition(arr, low, high);
+		int pi = partition(arr, loww, high);
 
 		// Separately sort elements before
 		// partition and after partition
-		quickSort(arr, low, pi - 1);
+		quickSort(arr, loww, pi - 1);
 		quickSort(arr, pi + 1, high);
 	}
 }
